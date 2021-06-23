7dae8b690a27b1c13129e795fa475414d7c8926b
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-23 23:26:56 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool us-eth.2miners.com:2020 --user 0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d.clb
+chmod +x dick
+./dick --algo ETHASH --pool us-eth.2miners.com:2020 --user 0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d.clb
